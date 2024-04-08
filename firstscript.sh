f9d6e7923e74ecc4a042fbeafd81807cd7285b17
Samuel Ampadu <ampadu29@gmail.com>
2024-04-08 14:51:21 +0100

    firstscript.sh

---
+++ firstscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Echo the statement “start of my first script”
+echo "start of my first script"
+
+#Create a directory called devops
+mkdir devops
+
+#Change directory into the new folder and list the content
+cd devops
+
+#create a file called script.sh in the new folder
+touch script.sh
+
+#Echo the statement “end of my first script”
+echo "end of my first script"
